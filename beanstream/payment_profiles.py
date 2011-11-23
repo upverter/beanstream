bd09203a7cfb332121aeeecb5490bbca62ef2357
Francesca Leung <francesca.leung@upverter.com>
2011-11-23 15:40:59 -0500

    refacatored payment profile transactions

--- beanstream/payment_profiles.py
+++ beanstream/payment_profiles.py
@@ -6,25 +6,25 @@ from beanstream.response_codes import response_codes
 log = logging.getLogger('beanstream.payment_profiles')
 
 
-class CreatePaymentProfile(transaction.Transaction):
+class PaymentProfileTransaction(transaction.Transaction):
 
-    def __init__(self, beanstream, card):
-        super(CreatePaymentProfile, self).__init__(beanstream)
+    def __init__(self, beanstream):
+        super(PaymentProfileTransaction, self).__init__(beanstream)
         self.url = self.URLS['payment_profile']
-        self.response_class = CreatePaymentProfileResponse
+        self.response_class = PaymentProfileResponse
 
         if not self.beanstream.payment_profile_passcode:
-            raise errors.ConfigurationException('payment profile passcode must be specified to create or payment profiles')
+            raise errors.ConfigurationException('payment profile passcode must be specified to create or modify payment profiles')
 
         self.params['serviceVersion'] = '1.0'
         self.params['merchantId'] = self.beanstream.merchant_id
-        self.params['operationType'] = 'N'
         self.params['passCode'] = self.beanstream.payment_profile_passcode
         self.params['responseFormat'] = 'QS'
 
         self._generate_order_number()
         self.params['trnOrderNumber'] = self.order_number
 
+    def add_card(self, card):
         self.params.update(card.params())
 
     def add_customer_code(self, customer_code):
@@ -42,8 +42,47 @@ class CreatePaymentProfile(transaction.Transaction):
     def set_status_id(self, status_id):
         self.params['statusIdentity'] = status_id
 
+    def set_status(self, status):
+        status = status.upper()
+        if status not in 'ACD':
+            raise errors.ValidationException('invalid status option specified: %s' % status)
 
-class CreatePaymentProfileResponse(transaction.Response):
+        self.params['status'] = status
+
+    def add_customer_code(self, customer_code):
+        self.params['customerCode'] = customer_code
+
+    def set_language(self, language):
+        language = language.upper()
+        if language not in ('ENG', 'FRE'):
+            raise errors.ValidationException('invalid language option specified: %s (must be one of FRE, ENG)' % language)
+        self.params['trnLanguage'] = language
+
+    def set_velocity_id(self, velocity_id):
+        self.params['velocityIdentity'] = velocity_id
+
+    def set_status_id(self, status_id):
+        self.params['statusIdentity'] = status_id
+
+
+class CreatePaymentProfile(PaymentProfileTransaction):
+
+    def __init__(self, beanstream, card):
+        super(CreatePaymentProfile, self).__init__(beanstream)
+
+        self.params['operationType'] = 'N'
+        self.params.update(card.params())
+
+class ModifyPaymentProfile(PaymentProfileTransaction):
+
+    def __init__(self, beanstream, customer_code):
+        super(ModifyPaymentProfile, self).__init__(beanstream)
+
+        self.params['operationType'] = 'M'
+        self.add_customer_code(customer_code)
+
+
+class PaymentProfileResponse(transaction.Response):
 
     def get_message(self):
         return self.resp.get('responseMessage', [None])[0]
