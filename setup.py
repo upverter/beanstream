d8d5e76bc89c03f9ce37b152a7355d3762828396
Francesca Leung <francesca.leung@upverter.com>
2011-11-24 10:27:01 -0500

    barebones setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='python-beanstream',
+    version='0.1',
+    description='Beanstream library',
+    packages=['beanstream'],
+)
