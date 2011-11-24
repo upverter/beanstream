9e0ee54a2a31cde2141c1ec380213d603f16b950
Francesca Leung <francesca.leung@upverter.com>
2011-11-23 21:45:26 -0500

    initial notifications parsing for recurring billing

---
+++ beanstream/notifications.py
@@ -0,0 +1,63 @@
+from beanstream import transaction
+from beanstream.response_codes import response_codes
+
+
+class RecurringBillingNotification(transaction.Response):
+
+    def account_id(self):
+        return self.resp.get('billingId', None)
+
+    def approved(self):
+        return self.resp.get('trnApproved', '0') == '1'
+
+    def transaction_id(self):
+        return self.resp.get('trnId', None)
+
+    def get_cardholder_message(self):
+        if 'messageId' in self.resp:
+            return response_codes[self.resp['messageId'][0]]['cardholder_message']
+        else:
+            return None
+
+    def get_merchant_message(self):
+        if 'messageId' in self.resp:
+            return response_codes[self.resp['messageId'][0]]['merchant_message']
+        else:
+            return None
+
+    def auth_code(self):
+        return self.resp.get('authCode', None)
+
+    def account_name(self):
+        return self.resp.get('accountName', None)
+
+    def email(self):
+        return self.resp.get('email', None)
+
+    def billing_amount(self):
+        return self.resp.get('billingAmount', None)
+
+    def billing_date(self):
+        return self.resp.get('billingDate', None)
+
+    def billing_period(self):
+        return self.resp.get('billingPeriod', None)
+
+    def billing_increment(self):
+        return self.resp.get('billingIncrement', None)
+
+    def period_from(self):
+        return self.resp.get('periodFrom', None)
+
+    def period_to(self):
+        return self.get('periodTo', None)
+
+    def refs(self):
+        return [
+            self.get('ref1', None),
+            self.get('ref2', None),
+            self.get('ref3', None),
+            self.get('ref4', None),
+            self.get('ref5', None),
+        ]
+
