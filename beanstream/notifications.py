fe1b10dece11c387e0f591256060cebafc1b796c
Francesca Leung <francesca.leung@upverter.com>
2011-11-29 11:36:21 -0500

    default in notification response should be a list of None

--- beanstream/notifications.py
+++ beanstream/notifications.py
@@ -11,13 +11,13 @@ class RecurringBillingNotification(transaction.Response):
         self.resp = dict((k, [v]) if type(v) != list else (k, v) for k, v in self.resp.iteritems())
 
     def account_id(self):
-        return self.resp.get('billingId', None)[0]
+        return self.resp.get('billingId', [None])[0]
 
     def approved(self):
         return self.resp.get('trnApproved', ['0'])[0] == '1'
 
     def transaction_id(self):
-        return self.resp.get('trnId', None)[0]
+        return self.resp.get('trnId', [None])[0]
 
     def get_cardholder_message(self):
         if 'messageId' in self.resp:
@@ -32,29 +32,29 @@ class RecurringBillingNotification(transaction.Response):
             return None
 
     def auth_code(self):
-        return self.resp.get('authCode', None)[0]
+        return self.resp.get('authCode', [None])[0]
 
     def name(self):
-        return self.resp.get('accountName', None)[0]
+        return self.resp.get('accountName', [None])[0]
 
     def email(self):
-        return self.resp.get('emailAddress', None)[0]
+        return self.resp.get('emailAddress', [None])[0]
 
     def billing_amount(self):
-        return self.resp.get('billingAmount', None)[0]
+        return self.resp.get('billingAmount', [None])[0]
 
     def billing_date(self):
-        return self.resp.get('billingDate', None)[0]
+        return self.resp.get('billingDate', [None])[0]
 
     def billing_period(self):
-        return self.resp.get('billingPeriod', None)[0]
+        return self.resp.get('billingPeriod', [None])[0]
 
     def billing_increment(self):
-        return self.resp.get('billingIncrement', None)[0]
+        return self.resp.get('billingIncrement', [None])[0]
 
     def period_from(self):
-        return self.resp.get('periodFrom', None)[0]
+        return self.resp.get('periodFrom', [None])[0]
 
     def period_to(self):
-        return self.resp.get('periodTo', None)[0]
+        return self.resp.get('periodTo', [None])[0]
 
