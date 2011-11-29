dc5d968b87ac037a646c48e0ad23281dead29e0d
Francesca Leung <francesca.leung@upverter.com>
2011-11-29 17:34:32 -0500

    parameters to date constructor must be ints

--- beanstream/utilities.py
+++ beanstream/utilities.py
@@ -3,4 +3,4 @@ from datetime import date
 def process_date(datestring):
     """ 11/29/2011 --> date(2011, 11, 29) """
     month, day, year = datestring.split('/')
-    return date(year, month, day)
+    return date(int(year), int(month), int(day))
